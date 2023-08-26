f2e31017c403ef1151701e4c7f01a36f1db81e4c
Genov10 <genovolexandr10@gmail.com>
2023-08-26 14:59:46 +0300

    I have done all tasks. I decided to do game without input parameter to avoid overloading the main func

---
+++ src/edu/hw7/Main.java
@@ -0,0 +1,21 @@
+package edu.hw7;
+
+public class Main {
+    public static void main(String[] args) {
+        Methods methods = new Methods();
+
+        String speech = "A speech with some symbols";
+        char symbol = 's';
+        String target1 = "ith some";
+        String target2 = "asdfghjkl";
+        String word1 = "abc";
+        String word2 = "tenet";
+        methods.findSymbolOccurance(speech, symbol);
+        methods.findWordPosition(speech, target1);
+        methods.findWordPosition(speech, target2);
+        Methods.reversedString(speech);
+        Methods.isPalendrom(word1);
+        Methods.isPalendrom(word2);
+        Methods.game();
+    }
+}
